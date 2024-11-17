7b7b691802661fedf937cf0be22e6f4c42fc3b01
Stanisław Horna <stanislawhorna@outlook.com>
2024-11-17 22:41:26 +0100

    add update method

--- src/DB/BaseDBOpsModel.py
+++ src/DB/BaseDBOpsModel.py
@@ -22,6 +22,25 @@ class BaseDBOpsModel:
                 data[field] = None
         return model_class(**data)
 
+    def insert(self, session: Session):
+        session.add(self)
+        session.commit()
+        session.refresh(self)
+
+    def update(self, session: Session, values_to_update: Type[T]):
+        self.__update_model_fields(values_to_update)
+        session.add(self)
+        session.commit()
+        session.refresh(self)
+
+    def __update_model_fields(self, values_to_update: Type[T]):
+        fields = self.__get_model_fields(self.__class__)
+        for f in fields:
+            value = getattr(values_to_update, f)
+            # if value is None, then we want to keep the existing value
+            if value and f != "id":
+                setattr(self, f, value)
+
     @staticmethod
     def __get_model_fields(model_class: Type[T]) -> list[str]:
         return list(model_class.__dict__.get("model_fields").keys())
@@ -29,8 +48,3 @@ class BaseDBOpsModel:
     @staticmethod
     def __get_type(model_class: Type[T], field_name: str) -> type:
         return model_class.__annotations__.get(field_name)
-
-    def insert(self, session: Session):
-        session.add(self)
-        session.commit()
-        session.refresh(self)
