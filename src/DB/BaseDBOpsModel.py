7eb5cdf9631848a53e3447a67476215b177d6adc
Stanisław Horna <stanislawhorna@outlook.com>
2024-11-17 21:05:08 +0100

    unify table col names

--- src/DB/BaseDBOpsModel.py
+++ src/DB/BaseDBOpsModel.py
@@ -1,21 +1,35 @@
 from typing import Type, TypeVar
 from sqlalchemy.orm import Session
-import src.Utils as Utils
 
 
-T = TypeVar('T')
+T = TypeVar("T")
+
 
 class BaseDBOpsModel:
-    
+
     @staticmethod
     def create_model(model_class: Type[T], proto_request) -> T:
-        field_names = Utils.get_class_fields(model_class)
+        field_names = BaseDBOpsModel.__get_model_fields(model_class)
         data = {}
         for field in field_names:
-            if hasattr(proto_request, field):
-                data[field] = getattr(proto_request, field)
+            if not hasattr(proto_request, field):
+                continue
+            if getattr(proto_request, field):
+                data[field] = BaseDBOpsModel.__get_type(model_class, field)(
+                    getattr(proto_request, field)
+                )
+            else:
+                data[field] = None
         return model_class(**data)
-    
+
+    @staticmethod
+    def __get_model_fields(model_class: Type[T]) -> list[str]:
+        return list(model_class.__dict__.get("model_fields").keys())
+
+    @staticmethod
+    def __get_type(model_class: Type[T], field_name: str) -> type:
+        return model_class.__annotations__.get(field_name)
+
     def insert(self, session: Session):
         session.add(self)
         session.commit()
--- src/DB/Tables/Wallet.py
+++ src/DB/Tables/Wallet.py
@@ -1,10 +1,22 @@
 from sqlmodel import SQLModel, Field
+from pydantic import field_validator
+from src.DB.BaseDBOpsModel import BaseDBOpsModel
 
 
-class Wallet(SQLModel, table=True):
+class Wallet(SQLModel, BaseDBOpsModel, table=True):
     __tablename__: str = "wallet"
 
-    ID: int = Field(primary_key=True, default=None)
-    user_ID: int = Field(foreign_key="user.ID", nullable=False)
+    id: int = Field(primary_key=True, default=None)
+    user_id: int = Field(foreign_key="user.ID", nullable=False,)
     currency: str = Field()
     value: float = Field()
+
+    def __model_post_init__(self):
+        self.id = int(self.id)
+        self.user_id = int(self.user_id)
+    
+    # @field_validator("id", "user_id", mode="before")
+    @field_validator("user_id", mode="plain")
+    def parse_int(cls, value, field):
+        print("user_id")
+        return int(value)
