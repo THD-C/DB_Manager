bad30c513edaf2b8001aa4c894f23d7ab3c9ae27
Stanisław Horna <stanislawhorna@outlook.com>
2024-11-17 22:51:31 +0100

    cleanup

--- src/DB/Tables/Wallet.py
+++ src/DB/Tables/Wallet.py
@@ -1,5 +1,4 @@
 from sqlmodel import SQLModel, Field
-from pydantic import field_validator
 from src.DB.BaseDBOpsModel import BaseDBOpsModel
 
 
@@ -7,16 +6,9 @@ class Wallet(SQLModel, BaseDBOpsModel, table=True):
     __tablename__: str = "wallet"
 
     id: int = Field(primary_key=True, default=None)
-    user_id: int = Field(foreign_key="user.ID", nullable=False,)
+    user_id: int = Field(
+        foreign_key="user.ID",
+        nullable=False,
+    )
     currency: str = Field()
     value: float = Field()
-
-    def __model_post_init__(self):
-        self.id = int(self.id)
-        self.user_id = int(self.user_id)
-    
-    # @field_validator("id", "user_id", mode="before")
-    @field_validator("user_id", mode="plain")
-    def parse_int(cls, value, field):
-        print("user_id")
-        return int(value)
