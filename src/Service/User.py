e449988aa3f154fb617a769e333cb8a887fc9ef0
Stanislaw Horna <stanislawhorna@outlook.com>
2024-11-19 23:16:42 +0100

    feature/update delete user (#10)
    * add skip_commit on insert and update
    
    * add grpc mode creation based on dict and class data input
    
    * add new rpc GetUserDetails, Update, Delete
    
    * fix formatting

--- src/Service/User.py
+++ src/Service/User.py
@@ -1,8 +1,18 @@
 from user.user_pb2_grpc import UserServicer
-from user.user_pb2 import RegResponse, AuthResponse, AuthUser
+from user.user_pb2 import (
+    RegResponse,
+    AuthResponse,
+    AuthUser,
+    ReqUpdateUser,
+    ReqDeleteUser,
+    ReqGetUserDetails,
+    UserDetails,
+    ResultResponse,
+)
 from sqlalchemy import or_
 
 import src.DB as DB
+import src.Utils as Utils
 
 
 class User(UserServicer):
@@ -13,12 +23,15 @@ class User(UserServicer):
         user = DB.User.create_model(DB.User, request)
         try:
             with DB.Session() as s:
+                user.insert(s)
                 user_details.insert(s)
                 user.user_detail_ID = user_details.ID
-                user.insert(s)
-        except:
+                user.update(s, user)
+            return RegResponse(success=True)
+        except Exception as e:
+            print(e)
+
             return RegResponse(success=False)
-        return RegResponse(success=True)
 
     def Authenticate(self, request: AuthUser, context):
         with DB.Session() as s:
@@ -42,3 +55,84 @@ class User(UserServicer):
             )
 
         return AuthResponse(success=False)
+
+    def GetUserDetails(self, request: ReqGetUserDetails, context):
+        with DB.Session() as s:
+            db_user = s.query(DB.User).filter(DB.User.ID == request.id).first()
+            if db_user is None:
+                return UserDetails()
+
+            db_user_detail = (
+                s.query(DB.UserDetail)
+                .filter(DB.UserDetail.ID == db_user.user_detail_ID)
+                .first()
+            )
+
+            return Utils.create_grpc_model(
+                UserDetails,
+                {
+                    **db_user_detail.model_dump(),
+                    "username": db_user.username,
+                    "email": db_user.email,
+                },
+            )
+
+    def Update(self, request: ReqUpdateUser, context):
+        with DB.Session() as s:
+            db_user = s.query(DB.User).filter(DB.User.ID == request.id).first()
+
+            if db_user is None:
+                return ResultResponse(success=False)
+
+            if request.email or request.password:
+                try:
+                    db_user.update(s, DB.User.create_model(DB.User, request))
+                    return ResultResponse(success=True, id=db_user.ID)
+                except Exception as e:
+                    print(e)
+                    pass
+            else:
+                try:
+                    db_user_detail = (
+                        s.query(DB.UserDetail)
+                        .filter(DB.UserDetail.ID == db_user.user_detail_ID)
+                        .first()
+                    )
+                    db_user_detail.update(
+                        s, DB.UserDetail.create_model(DB.UserDetail, request)
+                    )
+                    return ResultResponse(success=True, id=str(db_user.ID))
+                except Exception as e:
+                    print(e)
+                    pass
+
+        return ResultResponse(success=False)
+
+    def Delete(self, request: ReqDeleteUser, context):
+        try:
+            with DB.Session() as s:
+                db_user = (
+                    s.query(DB.User)
+                    .filter(
+                        DB.User.ID == int(request.id),
+                        DB.User.email == request.mail,
+                        DB.User.password == request.password,
+                    )
+                    .first()
+                )
+                if db_user is None:
+                    return ResultResponse(success=False)
+
+                db_user_detail = (
+                    s.query(DB.UserDetail)
+                    .filter(DB.UserDetail.ID == db_user.user_detail_ID)
+                    .first()
+                )
+
+                db_user_detail.delete(s)
+                db_user.delete(s)
+                return ResultResponse(success=True, id=request.id)
+        except Exception as e:
+            print(e)
+
+        return ResultResponse(success=False)
