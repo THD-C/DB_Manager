9e5e784f12045ac7697f75f0598a819084eac8ff
Stanislaw Horna <stanislawhorna@outlook.com>
2024-12-15 22:44:28 +0000

    add table definition

--- src/DB/__init__.py
+++ src/DB/__init__.py
@@ -5,6 +5,7 @@ from src.DB.Tables.Transaction import Transaction
 from src.DB.Tables.User import User
 from src.DB.Tables.UserDetail import UserDetail
 from src.DB.Tables.Wallet import Wallet
+from src.DB.Tables.Payment import Payment
 from src.DB.engine import engine
 from src.DB.startup import create_tables
 
