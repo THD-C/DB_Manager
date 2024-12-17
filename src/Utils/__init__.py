2edb091ad2edbf8b74f24ed36b7d9a6da40532bc
Stanislaw Horna <stanislawhorna@outlook.com>
2024-12-17 21:27:51 +0100

    Feature/add traces (#25)
    * add recording handled exceptions
    
    * add class decorator for traces
    
    * instrument grpc server
    
    * update requirements.txt

--- src/Utils/__init__.py
+++ src/Utils/__init__.py
@@ -1,3 +1,5 @@
-# import src.Utils.OpenTelemetry.OpenTelemetry as OTEL
+import src.Utils.OpenTelemetry.OpenTelemetry as oTEL
+from src.Utils.OpenTelemetry.TraceMeta import TraceMeta
+from src.Utils.OpenTelemetry.TraceMeta import record_trace_exception
 from src.Utils.grpc_model_helper import create_grpc_model
 from src.Utils.grpc_model_helper import create_grpc_list_model
