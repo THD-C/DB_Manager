4810c95b186070be67a8428c54c6a887e024cdc6
Stanisław Horna <stanislawhorna@outlook.com>
2025-01-01 18:41:21 +0100

    fix creating diagram

--- DB_diagram_generator.py
+++ DB_diagram_generator.py
@@ -3,13 +3,14 @@ import subprocess
 from eralchemy import render_er
 from src.DB.startup import SQLModel
 
+
 def main():
-    if os.path.exists('ERD.png'):
-        os.remove('ERD.png')
-    render_er(SQLModel.metadata, 'erd.dot')
-    subprocess.run(['dot', '-Tpng', 'erd.dot', '-o', 'ERD.png'])
-    os.remove('erd.dot')
+    if os.path.exists("ERD.png"):
+        os.remove("ERD.png")
+    render_er(SQLModel.metadata, "erd.dot")
+    subprocess.run(["dot", "-Tpng", "erd.dot", "-o", "ERD.png"])
+    os.remove("erd.dot")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
